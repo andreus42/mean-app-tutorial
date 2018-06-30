3fa6f745e0874d7d12424565d2769e2198cd111d
Andrew Metell <andrew.metell@gmail.com>
2018-06-30 12:33:29 -0400

    8. Display Book List using Angular 6 Component and Material

---
+++ app.module.ts
@@ -0,0 +1,30 @@
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule } from "@angular/material";
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+imports: [
+  RouterModule.forRoot(appRoutes),
+  BrowserModule,
+  FormsModule,
+  ReactiveFormsModule,
+  HttpClientModule,
+  BrowserAnimationsModule,
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule
+],
